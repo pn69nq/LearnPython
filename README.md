59962b03f5e6e39a7f25da56ef0be58177e7b3ed
pn69nq <baichi10086@sina.com>
2018-01-31 18:36:43 +0800

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+##itle######
+asdfasdfasdfasdfasdfasdf
+
---
+++ init.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+echo "init python en"
+#pip install virtualenv
+#virtualenv -p /usr/bin/python3 py3env
+#virtualenv -p /usr/bin/python py2env
+./py3env/bin/activate
